fd73052e6258d146a8bb04f34a59895976748c7c
Marco Gandi <elgandoz@gmail.com>
2023-05-22 23:46:11 +0200

    Better PostCss

--- .eleventy.js
+++ .eleventy.js
@@ -3,22 +3,9 @@ const CleanCSS = require("clean-css");
 const UglifyJS = require("uglify-js");
 const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const postCss = require('postcss');
+const postcss = require('postcss');
 const postcssNesting = require('postcss-nesting');
 const autoprefixer = require('autoprefixer');
-
-const postcssFilter = (cssCode, done) => {
-	// we call PostCSS here.
-	postCss([postcssNesting(), autoprefixer()])
-		.process(cssCode, {
-			// path to our CSS file
-			from: '_includes/assets/css/inline.css'
-		})
-		.then(
-			(r) => done(null, r.css),
-			(e) => done(e, null)
-		);
-};
 
 module.exports = function(eleventyConfig) {
 
@@ -63,8 +50,17 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
   });
 
-  // eleventyConfig.addWatchTarget('./src/_includes/styles/tailwind.css');
-	eleventyConfig.addNunjucksAsyncFilter("postcss", postcssFilter);
+  // PostCSS => https://github.com/11ty/eleventy/issues/518#issuecomment-489033990
+  eleventyConfig.addNunjucksAsyncFilter("postcss", function(cssCode, callback) {
+    postcss([
+      postcssNesting,
+      autoprefixer,
+    ])
+      .process(cssCode)
+      .then(function (result) {
+        callback(null, result.css);
+      });
+  });
 
   // Minify CSS
   eleventyConfig.addFilter("cssmin", function(code) {
@@ -104,19 +100,20 @@ module.exports = function(eleventyConfig) {
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
   let markdownItAnchor = require("markdown-it-anchor");
-  let markdownItGHeadings = require("markdown-it-github-headings");
+  // let markdownItGHeadings = require("markdown-it-github-headings");
   let options = {
     html: true,  // Enable HTML tags in source
     breaks: true, // Convert line breaks into <br>
     linkify: true // Autoconvert URLs to links
   };
   let opts = {
-    permalink: false
+    permalink: true,
+    permalinkSymbol: '<svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>'
   };
 
   eleventyConfig.setLibrary("md", markdownIt(options)
     .use(markdownItAnchor, opts)
-    .use(markdownItGHeadings)
+    // .use(markdownItGHeadings)
   );
 
   return {
